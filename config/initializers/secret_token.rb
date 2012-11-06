872ce8a9d5e268fe491248a358c12443b06c2dea
ricardo <ricardo.j.bertolin@gmail.com>
2012-11-06 17:10:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetsApp::Application.config.secret_token = 'ffb914e802f8b152595e938d3849af2f5b561dd228360a3f65a74ef506c61a8df2a98d1683568c5cfcd4672a78bb8f8b345931870f25011e281118971f4f709e'
